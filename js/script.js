1f2b6d4ff29e0e6481fa28f89013b04186a32778
franckonline <vidoc89@gmail.com>
2021-08-13 18:35:22 +0200

    players semble reglé

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
 window.onload = () => {
     let joueuractif = "1";
+    function changeplayers() {
+        if (joueuractif == 1) {
+            joueuractif = 2;
+        } else {
+            joueuractif = 1;
+        }
+    }
+    let paschanger = 0;
     document.querySelector(".bip").addEventListener("click", checkpass);
     let scorePlayerA = 0;
     let scorePlayerB = 0;
     let cadrePlayerA = document.querySelector(".playerA");
     let cadrePlayerB = document.querySelector(".playerB");
-    // console.log("cadrePlayerA: ", cadrePlayerA);
-    // console.log("cadrePlayerB: ", cadrePlayerB);
-    let zones = document.querySelectorAll(".bip");
-    let nbrShoot = zones.length - 1;
 
+    let zones = document.querySelectorAll(".bip");
     zones.forEach(element => {
         element.addEventListener('click', checkpass)
     });
-    document.querySelector(".shoot").innertext = nbrShoot;
 
     function checkpass() {
+        console.log("passage en keepass");
+        paschanger = 0;
         let checksum1 = 0;
         let checksum2 = 0;
-        changeplayers();
         let unautre = this.classList;
+        if (unautre[4] != "coul0") {
+            alert("Attention déjà cliqué");
+        }
         let typetrait = unautre[0];
         let maCouleurClic = Number(unautre[4].slice(4));
         let colonne = Number(unautre[2].slice(3));
@@ -30,168 +38,94 @@ window.onload = () => {
         let ligneplusplus = Ligne + 2;
         let colonneplus = colonne + 1;
         let colonnemoins = colonne - 1;
-        let passbloc_sup_inf = 0;
-
         ///////******************************************************//////////////
-
-        console.log("le bip cliqué ", unautre);
-        function controltraitsup() {
-            checksum1 = 0;
-            // mode = trait
-            console.log("init check1 ");
-            let bipunstring = `.bloc.line${lignemoins}.col${colonnemoins}`;
-            let bipun = document.querySelector(bipunstring);
-            console.log("couleur sup gauche ", Number(bipun.classList[4].slice(4)));
-            if (Number(bipun.classList[4].slice(4)) >= 1) {
-                checksum1 = checksum1 + 1;
+        if (unautre[0] = "trait") {
+            if (typetrait == "trait" && maCouleurClic < 1) {
+                this.classList.replace("coul0", `coul${joueuractif}`);
+                if (Ligne > 1 && Ligne < 33) {
+                    controltraitsup();
+                    controltraitinf();
+                }
+                if (Ligne === 1) {
+                    controltraitinf();
+                }
+                if (Ligne === 33) {
+                    controltraitsup();
+                }
             }
-            console.log("valeur check1 sup gauche", checksum1);
-
-            // capture    du trait sup
-            let bipdeuxstring = `.trait.line${lignemoinsmoins}.col${colonne}`;
-            let bipdeux = document.querySelector(bipdeuxstring);
-            console.log("couleur trait sup ", Number(bipdeux.classList[4].slice(4)));
-            if (Number(bipdeux.classList[4].slice(4)) >= 1) {
-                checksum1 = checksum1 + 1;
-            }
-
-            // capture  du trait sup droite
-            let biptroisstring = `.bloc.line${lignemoins}.col${colonneplus}`;
-            let biptrois = document.querySelector(biptroisstring);
-            console.log("valeur check1 sup droite", checksum1);
-            console.log("couleur trait sup droite ", Number(biptrois.classList[4].slice(4)));
-            if (Number(biptrois.classList[4].slice(4)) >= 1) {
-                checksum1 = checksum1 + 1;
-            }
-            console.log("number du sup droite  ", checksum1);
-            console.log("valeur tot sup check_", checksum1);
-
-            if (checksum1 == 3) {
-                console.log("dedans checksum1 sup controle_", checksum1);
+            
+            function controltraitsup() {
                 checksum1 = 0;
-                // console.log("je check");
-                let cellulesupstring = `.bloc.line${lignemoins}.col${colonne}.cell`;
-                celluledessus = document.querySelector(cellulesupstring);
-                // console.log("cell de dessus == ", celluledessus);
-
-                if (joueuractif === 1) {
-                    celluledessus.style.backgroundColor = "red";
-                    scorePlayerA++;
-                    passbloc_sup_inf = 1;
-                } else {
-                    celluledessus.style.backgroundColor = "blue";
-                    scorePlayerB++;
-                    // passbloc_sup_inf = 1;
+                let bipunstring = `.bloc.line${lignemoins}.col${colonnemoins}`;
+                let bipun = document.querySelector(bipunstring);
+                if (Number(bipun.classList[4].slice(4)) >= 1) {
+                    checksum1 = checksum1 + 1;
                 }
-
-                // changeplayers();
-
-            }
-
-        }
-
-        function controltraitinf() {
-            console.log("deuxieme checksum_2 ", checksum2);
-
-            // mode = trait
-
-            // capture  du trait inf droite
-            let bipquatrestring = `.bloc.line${ligneplus}.col${colonneplus}`;
-            let bipquatre = document.querySelector(bipquatrestring);
-            console.log("bipquatre ", bipquatre);
-            console.log("couleur trait inf droite ", Number(bipquatre.classList[4].slice(4)));
-            if (Number(bipquatre.classList[4].slice(4)) >= 1) {
-                checksum2 = checksum2 + 1;
-            }
-            console.log("number du inf droite  ", checksum2);
-
-            // capture    du trait inf
-            let bipcinqstring = `.trait.line${ligneplusplus}.col${colonne}`;
-            let bipcinq = document.querySelector(bipcinqstring);
-            console.log("couleur trait inferieur ", Number(bipcinq.classList[4].slice(4)));
-            if (Number(bipcinq.classList[4].slice(4)) >= 1) {
-                checksum2 = checksum2 + 1;
-            }
-            console.log("number du inferieur  ", checksum2);
-
-            // capture  du trait sup droite
-            let bipsixstring = `.bloc.line${ligneplus}.col${colonnemoins}`;
-            let bipsix = document.querySelector(bipsixstring);
-            console.log("couleur trait inf gauche ", Number(bipsix.classList[4].slice(4)));
-            if (Number(bipsix.classList[4].slice(4)) >= 1) {
-                checksum2 = checksum2 + 1;
-            }
-            console.log("number du inf gauche  ", checksum2);
-            console.log("valeur tot sup check2_", checksum2);
-
-            if (checksum2 === 3) {
-                console.log("check2 me passe dans checksum2 === 3 ");
-                checksum2 = 0;
-                console.log("je check_22");
-                let celluleinfstring = `.bloc.line${ligneplus}.col${colonne}.cell`;
-                celluledessous = document.querySelector(celluleinfstring);
-                console.log("cell de dessous == ", celluledessous);
-                celluledessous.style.backgroundColor = "pink";
-
-                if (joueuractif === 1) {
-                    celluledessous.style.backgroundColor = "red";
-                    scorePlayerA++;
-                    passbloc_sup_inf = 1;
-                } else {
-                    celluledessous.style.backgroundColor = "blue";
-                    scorePlayerB++;
-                    passbloc_sup_inf = 1;
+                let bipdeuxstring = `.trait.line${lignemoinsmoins}.col${colonne}`;
+                let bipdeux = document.querySelector(bipdeuxstring);
+                if (Number(bipdeux.classList[4].slice(4)) >= 1) {
+                    checksum1 = checksum1 + 1;
                 }
+                let biptroisstring = `.bloc.line${lignemoins}.col${colonneplus}`;
+                let biptrois = document.querySelector(biptroisstring);
+                if (Number(biptrois.classList[4].slice(4)) >= 1) {
+                    checksum1 = checksum1 + 1;
+                }
+                if (checksum1 == 3) {
+                    checksum1 = 0;
+                    paschanger = 1;
+                    let cellulesupstring = `.bloc.line${lignemoins}.col${colonne}.cell`;
+                    celluledessus = document.querySelector(cellulesupstring);
+                    if (joueuractif === 1) {
+                        celluledessus.style.backgroundColor = "red";
+                        scorePlayerA++;
+                    } else {
+                        celluledessus.style.backgroundColor = "blue";
+                        scorePlayerB++;
+                    }
+                }
+            }
 
-                changeplayers();
-               
-
+            function controltraitinf() {
+                let bipquatrestring = `.bloc.line${ligneplus}.col${colonneplus}`;
+                let bipquatre = document.querySelector(bipquatrestring);
+                if (Number(bipquatre.classList[4].slice(4)) >= 1) {
+                    checksum2 = checksum2 + 1;
+                }
+                let bipcinqstring = `.trait.line${ligneplusplus}.col${colonne}`;
+                let bipcinq = document.querySelector(bipcinqstring);
+                if (Number(bipcinq.classList[4].slice(4)) >= 1) {
+                    checksum2 = checksum2 + 1;
+                }
+                let bipsixstring = `.bloc.line${ligneplus}.col${colonnemoins}`;
+                let bipsix = document.querySelector(bipsixstring);
+                if (Number(bipsix.classList[4].slice(4)) >= 1) {
+                    checksum2 = checksum2 + 1;
+                }
+                if (checksum2 === 3) {
+                    paschanger = 1;
+                    checksum2 = 0;
+                    let celluleinfstring = `.bloc.line${ligneplus}.col${colonne}.cell`;
+                    celluledessous = document.querySelector(celluleinfstring);
+                    if (joueuractif === 1) {
+                        celluledessous.style.backgroundColor = "red";
+                        scorePlayerA++;
+                    } else {
+                        celluledessous.style.backgroundColor = "blue";
+                        scorePlayerB++;
+                    }
+                }
             }
         }
-
-
-        if (typetrait == "trait" && maCouleurClic < 1) {
-            this.classList.replace("coul0", `coul${joueuractif}`);
-            console.log("deuxieme checksum_2 ", checksum2);
-
-            // mode = trait
-
-                        // console.log("couleur >coul0 : ");
-            if (Ligne > 1 && Ligne < 33) {
-                controltraitsup();
-                controltraitinf();
-            }
-            if (Ligne === 1) {
-                controltraitinf();
-            }
-            if (Ligne === 33) {
-                controltraitsup();
-            }
-
-        } //(typetrait == "trait" && maCouleurClic < 1)----fin
-
+        /**
+         *  ****************  bloc ********************
+         */
         if (typetrait == "bloc") {
-
             this.classList.replace("coul0", `coul${joueuractif}`);
-
-            // console.log("couleur >coul0 : ");
-            if (colonne > 1 && colonne < 49) {
-                controltraitsup();
-                controltraitinf();
-            }
-            if (colonne === 1) {
-                controltraitinf();
-            }
-            if (colonne === 49) {
-                controltraitsup();
-
-            }
-
         }
-        let lastshoot = document.querySelector(".shoot");
-        document.querySelector(".shoot").innertext = nbrShoot;
-        lastshoot.textContent = (808 - nbrShoot) + "/ 808";
 
+        //gestion du shoot-------------------------------------
+        let lastshoot = document.querySelector(".shoot");
         if (scorePlayerA === scorePlayerB) {
             cadrePlayerA.style.color = "red";
             cadrePlayerB.style.color = "red";
@@ -204,16 +138,11 @@ window.onload = () => {
             cadrePlayerB.style.color = "green";
             cadrePlayerA.style.color = "red";
         }
-
         cadrePlayerA.textContent = scorePlayerA;
         cadrePlayerB.textContent = scorePlayerB;
-
-        function changeplayers() {
-            if (joueuractif == 1) {
-                joueuractif = 2
-            } else {
-                joueuractif = 1;
-            }
+        if (paschanger == 0) {
+            changeplayers();
         }
-    }
+        paschanger = 0;
+    }// fin zone keepass
 }
